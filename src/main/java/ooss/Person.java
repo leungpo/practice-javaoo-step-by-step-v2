df83d563f23b9a8f1328772092651b9acf23c5a8
leungpo <polly.leung@oocl.com>
2022-11-30 17:20:56 +0800

    feat: finish adding equal method(Step 1)

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -8,6 +8,21 @@ public class Person {
     private final int age;
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
     public Person(int id, String name, int age) {
         this.id = id;
         this.name = name;
