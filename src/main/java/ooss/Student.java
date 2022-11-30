09d61dd8da18bfa8418cf52adc4ceaa43dd2fa8c
leungpo <polly.leung@oocl.com>
2022-11-30 19:12:47 +0800

    feat: (Step 3) finish Student class

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,14 +1,28 @@
 package ooss;
 
 import java.text.MessageFormat;
+import java.util.Optional;
 
 public class Student extends Person{
+    private Klass klass;
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
     @Override
     public String introduce() {
-        return super.introduce() + " I am a student.";
+        String introduceString = super.introduce() + " I am a student.";
+        if(Optional.ofNullable(this.klass).isPresent()) {
+            introduceString += MessageFormat.format(" I am in class {0}.", klass.getNumber());
+        }
+        return introduceString;
+    }
+
+    public boolean isIn(Klass klass) {
+        return Optional.ofNullable(this.klass).isPresent()?this.klass.equals(klass): false;
+    }
+
+    public void join(Klass klass) {
+        this.klass = klass;
     }
 }
