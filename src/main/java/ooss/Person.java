e8123e5835e06389caa3c95aae56f73a5104e6b5
leungpo <polly.leung@oocl.com>
2022-11-30 17:17:44 +0800

    feat: finish Person and introduce method

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,4 +1,20 @@
 package ooss;
 
+import java.text.MessageFormat;
+
 public class Person {
+    private final int id;
+    private final String name;
+    private final int age;
+
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return MessageFormat.format("My name is {0}. I am {1} years old.", name, age);
+    }
 }
