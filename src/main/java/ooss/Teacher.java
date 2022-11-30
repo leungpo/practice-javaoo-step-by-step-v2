fcdef2391de635f3078a81e0e5bd38a739f3d1b7
leungpo <polly.leung@oocl.com>
2022-11-30 20:12:25 +0800

    feat: (Step 4) finish adding assignTo, belongsTo, isTeaching function

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,12 +1,37 @@
 package ooss;
 
-public class Teacher extends Person {
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+
+public class Teacher extends Person {
+    private List<Klass> klass = new ArrayList<>();
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
     @Override
     public String introduce() {
-        return super.introduce() + " I am a teacher.";
+        String introduceString = super.introduce() + " I am a teacher.";
+        String classString = "";
+        if (!this.klass.isEmpty()){
+            String klassString = klass.stream().map(klassObject -> klassObject.getNumber()).map(Object::toString).collect(Collectors.joining(", "));;
+            classString = MessageFormat.format(" I teach Class {0}.",klassString);
+        }
+        introduceString += classString;
+        return introduceString;
+    }
+    public void assignTo(Klass klass) {
+        this.klass.add(klass);
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return this.klass.isEmpty()? false: this.klass.contains(klass);
+    }
+
+    public boolean isTeaching(Student student) {
+        return this.klass.contains(student.getKlass());
     }
 }
